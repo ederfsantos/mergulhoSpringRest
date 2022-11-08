2100e6d917ecfd74fceda72bd59a08de3a6ab10f
EDER FERREIRA SANTOS <ederfsantos@live.com>
2022-11-08 13:46:33 -0300

    implementancao da classe clienteController e end points crud

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,59 +3,93 @@ package com.algaworks.algalog.api.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 @RestController
 @RequestMapping("/clientes")
+
 public class ClienteController {
-
-	// @GetMapping("/clientes")
-	// public String listar() {
-	// return "Teste";
-	// }
-    private List<Cliente> clientes = new ArrayList<Cliente>();
+	//@PersistenceContext
+	//private EntityManager manager;
 	
-@GetMapping
+	private ClienteRepository clienteRepository;
+	
+
+
+	@GetMapping
 	public List<Cliente> listar() {
-		// Cliente cliente1 = new Cliente();
-		var cliente1 = new Cliente();// no java 11 pode se usar essa anotação var
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("18 2222v-2222");
-		cliente1.setEmail("joao@email.com");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Jose");
-		cliente2.setTelefone("18 2225-2262");
-		cliente2.setEmail("jose@email.com");
-		this.clientes.add(cliente1);
-		this.clientes.add(cliente2);
-		return Arrays.asList(cliente1, cliente2);
+		
+	//return manager.createQuery("FROM Cliente",Cliente.class).getResultList();
+		return clienteRepository.findAll();
+    	//return clienteRepository.findByNomeContaining("R");
 	}
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		//Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		//return cliente.orElse(null);// se nao tiver nada, retorna null
+		//if(cliente.isPresent()) {
+	//		return ResponseEntity.ok(cliente.get());
+	//	}
+	//	return ResponseEntity.notFound().build();
+//	}
 
-	@GetMapping(value = "/{id}")
-	public Cliente buscarPorId(@PathVariable Long id) {
-		//var cliente1 = new Cliente();// no java 11 pode se usar essa anotação var
-	//	cliente1.setId(1L);
-		//cliente1.setNome("João");
-		//cliente1.setTelefone("18 2222v-2222");
-		//cliente1.setEmail("joao@email.com");
-		//return cliente1;
-		for(Cliente c:this.clientes) {
-			if(c.getId().equals(id)) {
-				return c;
-			}
-			
+//ou assim abaixo
+		
+		return clienteRepository.findById(clienteId)
+				//.map(cliente->ResponseEntity.ok(cliente)) //ou assim pra enxugar mais
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+		
+	}
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar( @PathVariable Long clienteId,
+			@RequestBody Cliente cliente){
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
 		}
-		return null;
-
+		cliente.setId(clienteId);//para poder atualizar o objeto
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+		
+	}
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		return ResponseEntity.noContent().build();
+	}
+	
 	}
 
-}
+
