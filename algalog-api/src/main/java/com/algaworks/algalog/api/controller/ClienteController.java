86423ce9b35adf70150341d0d26d4b88ffba66ef
EDER FERREIRA SANTOS <ederfsantos@live.com>
2022-11-09 13:55:37 -0300

    implementando camada de servico e validacao

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,14 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,73 +18,78 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.service.ClienteService;
 
 import lombok.AllArgsConstructor;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/clientes")
 
 public class ClienteController {
-	//@PersistenceContext
-	//private EntityManager manager;
-	
+	// @PersistenceContext
+	// private EntityManager manager;
+	// a injeção de dependecia aqui esta vindo pelo construtor anotação @AllArgs....
+	// acima
 	private ClienteRepository clienteRepository;
-	
 
+	private ClienteService clienteService;
 
 	@GetMapping
 	public List<Cliente> listar() {
-		
-	//return manager.createQuery("FROM Cliente",Cliente.class).getResultList();
+
+		// return manager.createQuery("FROM Cliente",Cliente.class).getResultList();
 		return clienteRepository.findAll();
-    	//return clienteRepository.findByNomeContaining("R");
+		// return clienteRepository.findByNomeContaining("R");
 	}
+
 	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
-		//Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-		
-		//return cliente.orElse(null);// se nao tiver nada, retorna null
-		//if(cliente.isPresent()) {
-	//		return ResponseEntity.ok(cliente.get());
-	//	}
-	//	return ResponseEntity.notFound().build();
+		// Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+		// return cliente.orElse(null);// se nao tiver nada, retorna null
+		// if(cliente.isPresent()) {
+		// return ResponseEntity.ok(cliente.get());
+		// }
+		// return ResponseEntity.notFound().build();
 //	}
 
 //ou assim abaixo
-		
+
 		return clienteRepository.findById(clienteId)
-				//.map(cliente->ResponseEntity.ok(cliente)) //ou assim pra enxugar mais
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+				// .map(cliente->ResponseEntity.ok(cliente)) //ou assim pra enxugar mais
+				.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
-		return clienteRepository.save(cliente);
-		
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		// return clienteRepository.save(cliente);
+		return clienteService.salvar(cliente);
 	}
+
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar( @PathVariable Long clienteId,
-			@RequestBody Cliente cliente){
-		
-		if(!clienteRepository.existsById(clienteId)) {
-			return ResponseEntity.notFound().build();
-		}
-		cliente.setId(clienteId);//para poder atualizar o objeto
-		cliente = clienteRepository.save(cliente);
-		return ResponseEntity.ok(cliente);
-		
-	}
-	@DeleteMapping("/{clienteId}")
-	public ResponseEntity<Void> remover(@PathVariable Long clienteId){
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @Valid @RequestBody Cliente cliente) {
+
 		if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
-		
-		clienteRepository.deleteById(clienteId);
+		cliente.setId(clienteId);// para poder atualizar o objeto
+		// cliente = clienteRepository.save(cliente);
+		cliente = clienteService.salvar(cliente);
+		return ResponseEntity.ok(cliente);
+
+	}
+
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		// clienteRepository.deleteById(clienteId);
+		clienteService.excluir(clienteId);
 		return ResponseEntity.noContent().build();
 	}
-	
-	}
 
-
+}
---
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.algaworks.algalog.domain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
+@Service
+public class ClienteService {
+	
+	private ClienteRepository clienteRepository;
+	
+	@Transactional //esta anotação funciona como se fosse um roolback ou tudo ou nada, desfaz a operação
+	public Cliente salvar(Cliente cliente) {
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+				.stream().anyMatch(clienteExistente->!clienteExistente.equals(cliente));
+		if(emailEmUso) {
+			throw new NegocioException("Já existe um cliente cadastrado com este email.");
+		}
+		
+		return clienteRepository.save(cliente);
+	}
+	@Transactional
+	public void excluir(Long clienteId) {
+		clienteRepository.deleteById(clienteId);
+	}
+
+}
